d222c5b3bdbb4adfe5db207ea67081068b5b2f09
Adel Bassiony <a.bassiony@icloud.com>
2020-09-15 18:49:04 +0200

    Added: Loader Component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = (props) => {
+    return (
+        <div className="loader centered-loader">
+            <h1>Loading...</h1>
+        </div>
+    );
+}
+
+export default Loader;
