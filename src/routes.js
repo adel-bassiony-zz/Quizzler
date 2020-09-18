f185a5ede1872a8891d7d7144e7cc5ec9b0e185f
Adel Bassiony <a.bassiony@icloud.com>
2020-09-18 18:01:06 +0200

    Added: Routes file

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 import React, {lazy} from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+// Lazy Components
+const Home = lazy(() => import('./components/Home/Home'));
+const Account = lazy(() => import('./components/Account/Account'));
+const Dashboard = lazy(() => import('./components/Dashboard/Dashboard'));
+const SetupQuiz = lazy(() => import('./components/SetupQuiz/SetupQuiz'));
+const Quiz = lazy(() => import('./components/Quiz/Quiz'));
+
 // Application Routes
 const Routes = (
     <Switch>
-        <Route path="/" exact component={<h1>Home Page</h1>} />
+        <Route path="/" exact component={Home} />
+        <Route path="/account" exact component={Account} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/start" exact component={SetupQuiz} />
+        <Route path="/quiz" exact component={Quiz} />
     </Switch>
 );
 
