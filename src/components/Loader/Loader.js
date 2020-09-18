fb592c0c7bbc231807d723132847de8d2061cd2c
Adel Bassiony <a.bassiony@icloud.com>
2020-09-18 18:01:37 +0200

    Improved: Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Loader = (props) => {
     return (
-        <div className="loader centered-loader">
-            <h1>Loading...</h1>
+        <div className="container">
+            <div className="row align-items-center" style={{minHeight: '800px'}}>
+                <div className="col text-center">
+                    <h1><strong>Quizzler</strong></h1>
+                    <p className="mt-3">Loading...</p>
+                </div>
+            </div>
         </div>
     );
 }
